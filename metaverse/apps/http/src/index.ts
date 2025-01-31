129ad439ebd83eb2d241dfad53a2e17bf8a35f29
Sahil Shangloo <sahilshangloo35@gmail.com>
2025-02-01 02:37:24 +0530

    Initial Repo Commit for metaverse-2D. Basic skeleton files and code created

---
+++ metaverse/apps/http/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { router } from './routes/v1';
+import { userRouter } from './routes/v1/user';
+import { spaceRouter } from './routes/v1/space';
+import { adminRouter } from './routes/v1/admin';
+import client from "@repo/db/client";
+
+const app = express();
+
+app.use("/api/v1/", router);
+app.use("/api/v1/", userRouter);
+app.use("/api/v1/", spaceRouter);
+app.use("/api/v1/", adminRouter);
+
+app.listen(process.env.PORT || 3000);
---
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+export const router = Router();
+
+router.post("/signup", (req, res) => {
+    res.json({
+        message: "Hello"
+    })
+});
+
+router.post("/signup", (req, res) => {
+
+});
+
+router.get("/avatars", (req, res) => {
+
+});
+
+router.get("/elements", (req, res) => {
+
+});
+
